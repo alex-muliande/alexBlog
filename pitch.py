0675335f1fd8ca4813667ebf8809f5427947a58a
alex-muliande <amuliande@gmail.com>
2019-09-15 16:25:37 +0300

    .Add routes for the log in and register forms.

--- pitch.py
+++ pitch.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '023cb1594e4c4ad9ae26d719b65b472f'
+
 
 posts = [
     {
@@ -26,6 +30,17 @@ def home():
 @app.route('/about')
 def about():
      return render_template('about.html', title='About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = RegistrationForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
