e60a07e0af004b7734277abf5191e402964da2de
alex-muliande <amuliande@gmail.com>
2019-09-16 12:03:42 +0300

    .Add a functionn to check if the log in form is working.

--- pitch.py
+++ pitch.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash,redirect
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '023cb1594e4c4ad9ae26d719b65b472f'
+
+
+
 
 
 posts = [
@@ -21,8 +25,8 @@ posts = [
     }
 ]
 
+# @app.route('/') 
 @app.route('/')
-@app.route('/home')
 def home():
     return render_template('home.html', posts=posts)
 
@@ -31,15 +35,24 @@ def home():
 def about():
      return render_template('about.html', title='About')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-    form = RegistrationForm()
-    return render_template('login.html', title='Login', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'alex@gmail.com' and form.password.data=='pass':
+            flash('You have been logged in!', 'Success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful.Please check Username and password', 'danger')
+    return render_template('login.html', title='Login', form=form )
 
 
 if __name__ == '__main__':
