45a27f3884be5dec13be15bd5fe61fc194602899
alex-muliande <amuliande@gmail.com>
2019-09-16 23:10:10 +0300

    Validate errors for username and emails that are already taken.

--- pitch/forms.py
+++ pitch/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from pitch.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20)])
@@ -9,6 +10,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
 
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first() 
+        if True:
+            raise ValidationError('That username is taken please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first() 
+        if True:
+            raise ValidationError('That email is taken please choose a different one.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
