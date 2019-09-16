2e180f8dbe46cf654394002d3e8d6b4632752fbb
alex-muliande <amuliande@gmail.com>
2019-09-16 21:32:24 +0300

    Add import value for bcrypt.and db.

--- pitch/__init__.py
+++ pitch/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 
@@ -9,5 +10,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '023cb1594e4c4ad9ae26d719b65b472f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from pitch import routes
