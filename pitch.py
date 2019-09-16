89c09aa952fd69311ef2d35951430082e95876f7
alex-muliande <amuliande@gmail.com>
2019-09-16 17:36:49 +0300

    Files restructuring.

--- pitch.py
+++ pitch.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+
 from flask import Flask, render_template, url_for, flash,redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
 
@@ -10,29 +11,7 @@ app.config['SECRET_KEY'] = '023cb1594e4c4ad9ae26d719b65b472f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(20),unique=True,nullable=False)
-    email = db.Column(db.String(20),unique=True,nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False) 
-    posts = db.relationship('Post', backref='author', lazy=True)
-
-    def __repr__ (self):
-        return f"User('{self.username}', '{self.email},{self.image_file}')"
-
-class Post(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String(100), nullable=False) 
-    date_posted = db.Column (db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-
-    def __repr__(self):
-        return f"Post ('{self.title}', '{self.date_posted},{self.image_file}')"
-      
-
+from models import User, Post
 
 
 posts = [
@@ -73,7 +52,7 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'paulwamaria@gmail.com' and form.password.data=='pass':
+        if form.email.data == 'alex@gmail.com' and form.password.data=='pass':
             flash('You have been logged in!', 'Success')
             return redirect(url_for('home'))
         else:
