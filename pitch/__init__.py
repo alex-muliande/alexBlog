20b972592ced978773b031dab62e6ba9d3319eca
alex-muliande <amuliande@gmail.com>
2019-09-17 07:50:02 +0300

    Add a message to redirect a user that is already loged in back to the home page.

--- pitch/__init__.py
+++ pitch/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 
@@ -11,5 +12,6 @@ app.config['SECRET_KEY'] = '023cb1594e4c4ad9ae26d719b65b472f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from pitch import routes
--- pitch/routes.py
+++ pitch/routes.py
@@ -1,26 +1,27 @@
-from flask import render_template, url_for, flash,redirect
-from pitch import app, db, bcrypt 
+from flask import render_template, url_for, flash, redirect
+from pitch import app, db, bcrypt
 from pitch.forms import RegistrationForm, LoginForm
 from pitch.models import User, Post
+from flask_login import login_user, current_user
 
 
 posts = [
     {
-        'author':'Alex Muliande',
-        'title':'Lamination',
-        'content':'First content post',
-        'date_posted':'September 15, 2018',
+        'author': 'Alex Muliande',
+        'title': 'Lamination',
+        'content': 'First content post',
+        'date_posted': 'September 15, 2018',
     },
     {
-        'author':'Mercy Pinky',
-        'title':'Lamination 2',
-        'content':'Second content post',
-        'date_posted':'September 18, 2019',
+        'author': 'Mercy Pinky',
+        'title': 'Lamination 2',
+        'content': 'Second content post',
+        'date_posted': 'September 18, 2019',
 
     }
 ]
 
-# @app.route('/') 
+# @app.route('/')
 @app.route('/')
 def home():
     return render_template('home.html', posts=posts)
@@ -28,27 +29,37 @@ def home():
 
 @app.route('/about')
 def about():
-     return render_template('about.html', title='About')
+    return render_template('about.html', title='About')
 
-@app.route('/register', methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username =form.username.data, email=form.email.data, password=hashed_password)
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login', methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'alex@gmail.com' and form.password.data=='pass':
-            flash('You have been logged in!', 'Success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful.Please check Username and password', 'danger')
-    return render_template('login.html', title='Login', form=form )
+            flash('Login Unsuccessful.Please check email and password', 'danger')            
+    return render_template('login.html', title='Login', form=form)
+
