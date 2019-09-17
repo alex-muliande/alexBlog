c66d48f522a1d0eadccea58a86b2ab745b311d9a
alex-muliande <amuliande@gmail.com>
2019-09-17 14:04:41 +0300

    Add a form to the account.html file

--- pitch/routes.py
+++ pitch/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect,request
 from pitch import app, db, bcrypt
-from pitch.forms import RegistrationForm, LoginForm
+from pitch.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from pitch.models import User, Post
 from flask_login import login_user, current_user, logout_user,login_required
 
@@ -72,6 +72,7 @@ def logout():
 @app.route('/account')
 @login_required
 def account():
+    form = UpdateAccountForm()
     image_file = url_for('static', filename='account_img/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form) 
     
