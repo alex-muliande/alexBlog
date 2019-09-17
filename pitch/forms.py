ebe1246762bb9104f69c63d29d3cb0a2b97c41c3
alex-muliande <amuliande@gmail.com>
2019-09-17 16:23:33 +0300

    Add update account info.

--- pitch/forms.py
+++ pitch/forms.py
@@ -14,12 +14,12 @@ class RegistrationForm(FlaskForm):
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first() 
-        if True:
+        if user:
             raise ValidationError('That username is taken please choose a different one.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first() 
-        if True:
+        if user:
             raise ValidationError('That email is taken please choose a different one.')
 
 class LoginForm(FlaskForm):
