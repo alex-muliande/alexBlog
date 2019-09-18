a539bee7827388fa03b924859cb0e70c268a93ae
alex-muliande <amuliande@gmail.com>
2019-09-18 17:31:47 +0300

    Add a class for requestingform reset.

--- pitch/routes.py
+++ pitch/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from pitch import app, db, bcrypt
-from pitch.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from pitch.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from pitch.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
