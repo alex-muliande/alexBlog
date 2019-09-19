60c51e030a4234dbac4093314b732a72b5007c6b
alex-muliande <amuliande@gmail.com>
2019-09-19 08:00:45 +0300

    Add email .

--- pitch/routes.py
+++ pitch/routes.py
@@ -147,7 +147,7 @@ def delete_post(post_id):
 def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message('Password Reset Request',
-                  sender='noreply@demo.com',
+                  sender='alexnad425@gmail.com',
                   recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
 {url_for('reset_token', token=token, _external=True)}
