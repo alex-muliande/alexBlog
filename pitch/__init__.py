cebdf76cacaf23caf0ef6f70202461e46651c30b
alex-muliande <amuliande@gmail.com>
2019-09-19 11:34:11 +0300

    Deployment

--- pitch/__init__.py
+++ pitch/__init__.py
@@ -1,17 +1,19 @@
 import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
-
+load_dotenv()
 
 app = Flask(__name__)
 
 
-app.config['SECRET_KEY'] = '023cb1594e4c4ad9ae26d719b65b472f'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -22,6 +24,6 @@ app.config['MAIL_PORT']= 587
 app.config['MAIL_USE_TLS']= True
 app.config['MAIL_USERNAME']= os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD']= os.environ.get('EMAIL_PASS')
-mail = mail (app)
+mail = Mail(app)
 
 from pitch import routes
