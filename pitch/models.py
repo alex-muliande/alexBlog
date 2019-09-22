7030e8f166656932ccefbf7ed25abb3fd847dda6
alex-muliande <amuliande@gmail.com>
2019-09-22 19:14:25 +0300

    update deployment.

--- pitch/models.py
+++ pitch/models.py
@@ -13,6 +13,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20),unique=True,nullable=False)
     email = db.Column(db.String(20),unique=True,nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False) 
     posts = db.relationship('Post', backref='author', lazy=True)
     comment = db.relationship('Comment',backref = 'author',lazy=True)
@@ -31,8 +32,9 @@ class User(db.Model, UserMixin):
             return None
         return User.query.get(user_id)
 
+
     def __repr__ (self):
-        return f"User('{self.username}', '{self.email}')"
+        return f"User('{self.username}', '{self.email},{self.image_file}')"
 
 class Post(db.Model):
     id = db.Column(db.Integer,primary_key=True)
