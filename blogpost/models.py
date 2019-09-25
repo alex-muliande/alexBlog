c7ad06f8ed556f41409cc0dd6c3e7cef1233ff6e
alex-muliande <amuliande@gmail.com>
2019-09-25 14:45:27 +0300

    Deployment.

--- blogpost/models.py
+++ blogpost/models.py
@@ -3,7 +3,7 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from blogpost import db, login_manager,app 
 from flask_login import UserMixin
 from . import db
-    
+
 @login_manager.user_loader 
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -12,8 +12,8 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(55),unique=True,nullable=False)
-    email = db.Column(db.String(55),unique=True,nullable=False)
-    image_file = db.Column(db.Text(), nullable=False, default='https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+    email = db.Column(db.String(5555),unique=True,nullable=False)
+    image_file = db.Column(db.String(5555), nullable=False, default='https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
     password = db.Column(db.String(60), nullable=False) 
     posts = db.relationship('Post', backref='author', lazy=True)
     comment = db.relationship('Comment',backref = 'author',lazy=True)
